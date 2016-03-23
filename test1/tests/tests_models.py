62a38755fd8ca1656ba3d8408d74e122b7b0b633
James Wilkerson <jwilkerson@gmail.com>
2016-03-23 15:17:41 -0400

    Added some example model tests

--- test1/tests/tests_models.py
+++ test1/tests/tests_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 import datetime
+from nose.tools import nottest
 from ..models import Musician, Album, Instrument
 
 
@@ -9,41 +10,34 @@ class BaseEntries(TestCase):
         self.instrument = Instrument(instrument_type='Guitar')
         self.instrument.save()
         self.musician = Musician(
-        first_name='Jimi', last_name='Hendrix', instrument=self.instrument)
+            first_name='Jimi', last_name='Hendrix', instrument=self.instrument)
         self.musician.save()
         self.album = Album(artist=self.musician, name='Band of Gypsys',
-                       num_stars=5, release_date=datetime.date(year=1970, month=3, day=25))
+                           num_stars=5, release_date=datetime.date(year=1970, month=3, day=25))
         self.album.save()
 
 
 class MusicianModelTest(BaseEntries):
 
     def test_string_representation(self):
-
         self.assertEqual(str(self.musician), self.musician.last_name)
+
+    def test_musician_instrument_connection(self):
+        self.assertEqual(self.instrument.instrument_type,
+                         self.musician.instrument.instrument_type)
+
 
 class AlbumModelTest(BaseEntries):
 
     def test_string_representation(self):
         self.assertEqual(str(self.album), self.album.name)
 
-      # self.fail("TODO Test Incomplete")
-# from django.db.models import CharField
-# from base_tests import MusicianFactory, AlbumFactory
+    def test_album_musician_connection(self):
+        self.assertEqual(self.musician.last_name, self.album.artist.last_name)
 
 
-# class Tests():
-#     # Decorator required to allow test db creation
+class InstrumentModelTest(BaseEntries):
 
-#     @pytest.mark.django_db
-#     def test_create_one_factory_user(self):
-#         musician = MusicianFactory.build()
-#         assert musician.first_name == 'Jim'
-
-#     @pytest.mark.django_db
-#     def test_create_one_album(self):
-#         album = AlbumFactory.build()
-#         musician = MusicianFactory.build()
-#         musician.save()
-#         # print(album.name)
-#         assert album.name == "Eat A Bullet"
+    def test_string_representation(self):
+        self.assertEqual(str(self.instrument.instrument_type),
+                         self.instrument.instrument_type)
